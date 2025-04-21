1dc4ed4db38d715e5331a3be68d6d635af9d1f06
alyas-land <a.mashayekhi90@gmail.com>
2025-04-21 22:56:46 +0330

    Work for better ui login

--- lib/main.dart
+++ lib/main.dart
@@ -30,123 +30,203 @@ class _MyShopState extends State<MyShop> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Center
-          (child: Text("Khariday App",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 30,
-          fontWeight: FontWeight.bold
-        ),
-        ),
-        ),
-          backgroundColor: Colors.blueAccent,
-        ),
 
-        body: Container(
+      theme: ThemeData(
+        fontFamily: "Irancell"
+      ),
+      home: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment(0, 0), // near the top right
+            radius: 0.8,
+            colors: <Color>[
+              Color(0xFF145DA0), //  sun
+              Color(0xFF0C2D48), //  sky
+            ],
+            )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(title: Center
+            (child: Text("هکس آرنا",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          ),
+            backgroundColor: Color(0xFF0C2D48),
+          ),
 
-          child: ListView(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 30, 0, 10),
-                child: Center(
-                    child: Text("Login",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                    ),)),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(100, 10, 100, 1),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  controller: username,
-                  decoration: InputDecoration(
-                    errorText: errorUsername,
-                    hintText: "Username",
-                    contentPadding: EdgeInsets.fromLTRB(20, 1, 20, 1),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular((50))))
+          body: Container(
+
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 10),
+                  child: Column(
+                    children: [
+                      Image(image: AssetImage('assets/img/game-vector.png'),
+                      width: 300,
+                        height: 150,
+                      ),
+
+                      Center(
+                        child: Text("ورود به حساب کاربری",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFFFFFFF),
+                        ),
+                        ),
+                      ),
+                    ]
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(100, 20, 100, 1),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  controller: password,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Password",
-                      errorText: errorPassword,
+                Container(
+                  margin: EdgeInsets.fromLTRB(50, 10, 50, 1),
+                  child: TextField(
+
+                    textAlign: TextAlign.center,
+                    controller: username,
+                    decoration: InputDecoration(
+                      errorText: errorUsername,
+                      hintText: "نام کاربری",
+                        hintStyle: TextStyle(
+                          color: Color(0x9EFFFFFF),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400
+                        ),
                       contentPadding: EdgeInsets.fromLTRB(20, 1, 20, 1),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular((50))))
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(50)),
+                      borderSide: BorderSide(color: Color(0xFFFFFFFF), width: 2.0
+                      ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(
+                          color: Color(0xFFFFFFFF),
+                          width: 0.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(
+                          color: Color(0x9E0084FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      fillColor: Color(0x3BFFFFFF),
+                      filled: true,
+
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(100, 20, 100, 1),
-                child: MaterialButton(
-                    padding: EdgeInsets.fromLTRB(20, 12, 20, 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50))
+                Container(
+                  margin: EdgeInsets.fromLTRB(50, 20, 50, 1),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: password,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "رمز عبور",
+                        hintStyle: TextStyle(
+                          color: Color(0x9EFFFFFF),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400
+                        ),
+
+                        errorText: errorPassword,
+                      contentPadding: EdgeInsets.fromLTRB(20, 1, 20, 1),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(color: Color(0xFFFFFFFF), width: 2.0
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(
+                          color: Color(0xFFFFFFFF),
+                          width: 0.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(
+                          color: Color(0x9E0084FF),
+                          width: 1.5,
+                        ),
+                      ),
+                      fillColor: Color(0x3BFFFFFF),
+                      filled: true,
                     ),
-                    color: Colors.blue,
-                    child: Text("Sign in",
-                    style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(50, 20, 50, 1),
+                  child: MaterialButton(
+                      padding: EdgeInsets.fromLTRB(20, 12, 20, 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50))
+                      ),
+                      color: Color(0xFF0C2D48),
+                      child: Text("ارسال",
+                      style: TextStyle(color: Colors.white),),
 
-                    onPressed: (){
-                      setState(() {
-                        errorUsername = null;
-                        if (username.text.length < 3){
-                          errorUsername = "Username Format invalidli";
-                        }
-
-                        errorPassword = null;
-                        if (password.text.length < 4){
-                          errorPassword = "Password must bigger than 4 charackter";
-                        }
-
-                        else if (username.text == "admin" && password.text == "12345"){
-                          color = Colors.blueAccent;
-                          result = "Welcome";
-                        }
-                        else {
-                          result = "Error";
-                          color = Colors.redAccent;
+                      onPressed: (){
+                        setState(() {
+                          errorUsername = null;
+                          if (username.text.length < 3){
+                            errorUsername = "فرمت نام کاربری درست نمی باشد";
                           }
 
+                          errorPassword = null;
+                          if (password.text.length < 4){
+                            errorPassword = "رمز عبور باید بیشتر از 4 کاراکتر باشد.";
+                          }
 
-                      });
-                    }),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                child: Center(child: Text(result,
-                  style: TextStyle(
-                    color: color,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
+                          else if (username.text == "admin" && password.text == "12345"){
+                            color = Colors.blueAccent;
+                            result = "Welcome";
+                          }
+                          else {
+                            result = "Error";
+                            color = Colors.redAccent;
+                            }
+
+
+                        });
+                      }),
                 ),
-              ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: Center(child: Text(result,
+                    style: TextStyle(
+                      color: color,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                  ),
+                ),
 
-            ],
+              ],
+            ),
           ),
+            bottomNavigationBar: BottomNavigationBar(
+              backgroundColor: Color(0xFF0C2D48),
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Color(0xFF7F7F7F),
+              items:[
+                BottomNavigationBarItem(icon: Icon(Icons.login_sharp,), label: "حساب کاربری"),
+                BottomNavigationBarItem(icon: Icon(Icons.home_filled,), label: "خانه"),
+                BottomNavigationBarItem(icon: Icon(Icons.shopping_bag,), label: "سبد"),
+                // BottomNavigationBarItem(icon: Icon(Icons.account_balance), label: "Acount"),
+
+              ],
+              currentIndex: bottomNavigationSelector,
+              onTap: action,
+            ),
         ),
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.blueAccent,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white70,
-            items:[
-              BottomNavigationBarItem(icon: Icon(Icons.login_sharp,), label: "Login"),
-              BottomNavigationBarItem(icon: Icon(Icons.home_filled,), label: "Home"),
-              BottomNavigationBarItem(icon: Icon(Icons.shopping_bag,), label: "Bascket"),
-              // BottomNavigationBarItem(icon: Icon(Icons.account_balance), label: "Acount"),
-
-            ],
-            currentIndex: bottomNavigationSelector,
-            onTap: action,
-          ),
       ),
     );
   }
